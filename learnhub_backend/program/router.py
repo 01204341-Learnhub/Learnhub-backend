565e754446601c4016cd345f588d7f2942d12875
PAFFx <jj2jom@gmail.com>
2023-08-23 13:07:09 +0700

    add depend pagination parameters for /programs

--- learnhub_backend/program/router.py
+++ learnhub_backend/program/router.py
@@ -1,4 +1,7 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from typing import Annotated
+
+from dependencies import common_pagination_parameters
 
 
 router = APIRouter(
@@ -7,5 +10,5 @@ router = APIRouter(
         )
 
 @router.get("/", status_code=200)
-async def list_programs():
+async def list_programs(common_paginations: Annotated[dict, Depends(common_pagination_parameters)]):
     return {200: "OK"}
