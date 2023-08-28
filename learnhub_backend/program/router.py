d8c60b8df9704d62eff36a258dca33b776b85d07
PAFFx <jj2jom@gmail.com>
2023-08-28 20:36:23 +0700

    move router logic to services.py

--- learnhub_backend/program/router.py
+++ learnhub_backend/program/router.py
@@ -1,10 +1,9 @@
 from fastapi import APIRouter, Depends
 from typing import Annotated
-from pydantic import TypeAdapter
 
 from ..dependencies import common_pagination_parameters
-from .schemas import Programs_model,Program_model
-from .database import query_list_programs
+from .schemas import Programs_model
+from .services import list_programs_response
 
 
 router = APIRouter(
@@ -17,7 +16,5 @@ common_page_params = Annotated[dict, Depends(router.dependencies[0].dependency)]
 
 @router.get("/", status_code=200, response_model_exclude_none=True)
 def list_programs(common_paginations: common_page_params) -> Programs_model:
-        queried_programs = query_list_programs(common_paginations["skip"], common_paginations["limit"])
-        ta = TypeAdapter(list[Program_model])
-        response_body = Programs_model(programs=ta.validate_python(queried_programs))
+        response_body = list_programs_response(common_paginations["skip"],common_paginations["limit"])
         return  response_body
