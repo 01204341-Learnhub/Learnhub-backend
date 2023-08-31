69d42bf6ed46fd9521fa71d3049ee334200bf85d
PAFFx <jj2jom@gmail.com>
2023-08-31 20:27:34 +0700

    add exceptions

---
+++ learnhub_backend/program/exceptions.py
@@ -0,0 +1,12 @@
+from fastapi import HTTPException
+
+class Error():
+    bad_request = HTTPException(400, detail="Bad Request")
+    unauthorized = HTTPException(401, detail="Unauthorized") 
+    forbidden = HTTPException(403, detail="Forbidden") # unlike unauthorized the client's identity is known.
+    not_found = HTTPException(404, detail="Not Found")
+    method_not_allowed = HTTPException(405, detail="Method Not Allowed")
+    request_timeout = HTTPException(408, detail="Request Timeout")
+    teapot = HTTPException(418, detail="I'm a teapot")
+
+
