29da5af008d42cd63ec09b97589feb46567369cd
PAFFx <jj2jom@gmail.com>
2023-08-31 19:21:56 +0700

    add service list_lessons_response

--- learnhub_backend/program/services.py
+++ learnhub_backend/program/services.py
@@ -1,12 +1,24 @@
 from typing import Annotated, Union
 from pydantic import TypeAdapter
 
-from .database import query_list_programs
-from .schemas import ListProgramsModel, ListProgramsCourseModel, ListProgramsClassModel
+from .database import (
+                query_list_programs,
+                query_list_lessons
+                )
+from .schemas import (
+                ListProgramsModel, ListProgramsCourseModel, ListProgramsClassModel,
+                ListLessonsModel, ListLessonsModelBody
+                      )
 
 
 def list_programs_response(skip: int = 0, limit : int = 0) -> ListProgramsModel:
         queried_programs = query_list_programs(skip, limit)
         ta = TypeAdapter(list[Union[ListProgramsClassModel, ListProgramsCourseModel]])
         response_body = ListProgramsModel(programs=ta.validate_python(queried_programs))
+        return response_body
+
+def list_lessons_response(skip: int=0, limit : int =0)-> ListLessonsModel:
+        quried_lessons = query_list_lessons(skip, limit)
+        ta = TypeAdapter(list[ListLessonsModelBody])
+        response_body = ListLessonsModel(lessons = ta.validate_python(quried_lessons))
         return response_body
