d22697b2b81bf902eef0f3a606dc036e2a3dcc66
PAFFx <jj2jom@gmail.com>
2023-08-31 20:35:54 +0700

    remove unused response

--- learnhub_backend/program/router.py
+++ learnhub_backend/program/router.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, Response
+from fastapi import APIRouter, Depends
 from typing import Annotated, Union
 
 from ..dependencies import common_pagination_parameters
@@ -29,7 +29,7 @@ def list_lessons(course_id: str, chapter_id: str, common_paginations: common_pag
         return response_body
 
 @router.get("/courses/{course_id}/chapters/{chapter_id}/lessons/{lesson_id}", status_code=200, response_model_exclude_none=True)
-def get_lesson(course_id: str, chapter_id: str, lesson_id : str, response : Response):
+def get_lesson(course_id: str, chapter_id: str, lesson_id : str):
         response_body = get_lesson_response(lesson_id)
         if response_body == None:
                 raise Exception.not_found
