3759862cc37b2a0660da03ec3f75dc0ee7956f75
PAFFx <jj2jom@gmail.com>
2023-08-31 19:27:18 +0700

    fix miss type bug and and user coll

--- learnhub_backend/database.py
+++ learnhub_backend/database.py
@@ -1,5 +1,5 @@
 from pymongo import MongoClient
-from .config import MONGODB_URI, MONGODB_DB_NAME, DB_COURSE_COLLECTION, DB_CHAPTER_COLLECTION, DB_LESSON_COLLECTION
+from .config import MONGODB_URI, MONGODB_DB_NAME, DB_COURSE_COLLECTION, DB_CHAPTER_COLLECTION, DB_LESSON_COLLECTION, DB_USER_COLLECTION
 
 
 
@@ -11,8 +11,9 @@ class DB_client(MongoClient):
 
         # add collection here
         self.course_coll = self.db[DB_COURSE_COLLECTION]
-        self.chapter_coll = self[DB_CHAPTER_COLLECTION]
-        self.lesson_coll = self[DB_LESSON_COLLECTION]
+        self.chapter_coll = self.db[DB_CHAPTER_COLLECTION]
+        self.lesson_coll = self.db[DB_LESSON_COLLECTION]
+        self.user_coll = self.db[DB_USER_COLLECTION]
 
 
 db_client = DB_client(MONGODB_URI)
