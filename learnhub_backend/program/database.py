161ebda715bdc8d0d2f0fe1537aa7a7351249775
PAFFx <jj2jom@gmail.com>
2023-08-31 20:14:55 +0700

    add db query_get_lesson

--- learnhub_backend/program/database.py
+++ learnhub_backend/program/database.py
@@ -1,4 +1,5 @@
 from ..database import db_client
+from bson import ObjectId
 
 def query_list_programs(skip: int = 0, limit: int = 100) -> list:
     courses_cursor = db_client.course_coll.find(skip=skip, limit=limit)
@@ -19,4 +20,11 @@ def query_list_lessons(skip: int = 0, limit: int = 100) -> list:
         lessons.append(lesson)
 
     return lessons
+
+def query_get_lesson(lesson_id : str) -> dict | None:
+    lesson_object_id = ObjectId(oid=lesson_id)
+    lesson = db_client.lesson_coll.find_one({"_id": lesson_object_id})
+    if lesson != None:
+         lesson["lesson_id"] = str(lesson["_id"])
+    return lesson
 
