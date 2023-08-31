f3b4504e537c781aa8f5b5001b2306bddb7289f7
PAFFx <jj2jom@gmail.com>
2023-08-31 19:22:08 +0700

    add router function

--- learnhub_backend/program/router.py
+++ learnhub_backend/program/router.py
@@ -3,7 +3,10 @@ from typing import Annotated, Union
 
 from ..dependencies import common_pagination_parameters
 from .schemas import ListProgramsModel
-from .services import list_programs_response
+from .services import (
+                list_programs_response,
+                list_lessons_response
+                       )
 
 
 router = APIRouter(
@@ -21,5 +24,6 @@ def list_programs(common_paginations: common_page_params) -> ListProgramsModel:
 
 @router.get("/courses/{course_id}/chapters/{chapter_id}/lessons", status_code=200, response_model_exclude_none=True)
 def list_lessons(course_id: str, chapter_id: str, common_paginations: common_page_params):
-        pass 
+        response_body = list_lessons_response(common_paginations["skip"], common_paginations["limit"])
+        return response_body
 
