aec1f3189425640183aecb7472ce36c7ded4ef2e
PAFFx <jj2jom@gmail.com>
2023-09-01 16:34:00 +0700

    add GenericOKResponse to main dependencies

--- learnhub_backend/dependencies.py
+++ learnhub_backend/dependencies.py
@@ -1,2 +1,9 @@
+from pydantic import BaseModel
+
+
 def common_pagination_parameters(skip: int = 0, limit: int = 100):
     return {"skip": skip, "limit": limit}
+
+
+class GenericOKResponse(BaseModel):
+    detail: str = "OK"
