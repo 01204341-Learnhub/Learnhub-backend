e82b02e363817cfdd709195f0bdb09a5d2e0ac12
PAFFx <jj2jom@gmail.com>
2023-09-19 21:48:24 +0700

    add teacher endpoints dir and router

--- learnhub_backend/main.py
+++ learnhub_backend/main.py
@@ -6,11 +6,13 @@ from .database import db_client
 from .program.router import router as program_router
 from .program.course.router import router as course_router
 from .student.router import router as student_router
+from .teacher.router import router as teacher_router
 
 app = FastAPI()
 app.include_router(program_router)
 app.include_router(course_router)
 app.include_router(student_router)
+app.include_router(teacher_router)
 
 
 try:
