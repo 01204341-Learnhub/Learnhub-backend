b21aae038c67a5420de1e7dd71d7c5a5a3adfd34
sinlnw <sinlnw4567@gmail.com>
2023-09-24 16:15:00 +0700

    list_classes without teacher, tags

--- learnhub_backend/program/classes/database.py
+++ learnhub_backend/program/classes/database.py
@@ -3,10 +3,21 @@ from ..database import db_client
 from bson.objectid import ObjectId
 from bson.errors import InvalidId
 from .schemas import (
-    placeholder
+    placeholder,
+    placeholder,
 )
 
 from ...dependencies import Exception
 
+
 def db_placeholder():
     return 0
+
+
+# CLASSES
+def query_list_classes(skip: int, limit: int):
+    try:
+        classes_corsor = db_client.class_coll.find(skip=skip, limit=limit)
+    except InvalidId:
+        raise Exception.bad_request
+    return classes_corsor
