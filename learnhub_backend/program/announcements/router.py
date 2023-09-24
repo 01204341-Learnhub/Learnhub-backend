e20e9e18f9fa59931ff9cebb6991540c5d019a7d
sinlnw <sinlnw4567@gmail.com>
2023-09-24 16:38:47 +0700

    move announcements to course

--- learnhub_backend/program/announcements/router.py
+++ learnhub_backend/program/announcements/router.py
@@ -1,22 +1,33 @@
 from typing import Annotated
 from fastapi import APIRouter, Depends
 
-from learnhub_backend.dependencies import GenericOKResponse, common_pagination_parameters, Exception
-from learnhub_backend.program.announcements.schemas import ListCourseAnnouncementsResponseModel, PostCourseAnnouncementRequestModel
-from learnhub_backend.program.announcements.services import create_course_announcements_response, list_course_announcements_response
+from learnhub_backend.dependencies import (
+    GenericOKResponse,
+    common_pagination_parameters,
+    Exception,
+)
+from ..schemas import (
+    ListCourseAnnouncementsResponseModel,
+    PostCourseAnnouncementRequestModel,
+)
+from ..services import (
+    create_course_announcements_response,
+    list_course_announcements_response,
+)
 
 
 router = APIRouter(
-    prefix="/programs/announcements",
+    prefix="/programs/courses",
     tags=["announcements"],
     dependencies=[
         Depends(common_pagination_parameters),
         Depends(GenericOKResponse),
         Depends(Exception),
-    ]
+    ],
 )
 
 common_page_params = Annotated[dict, Depends(router.dependencies[0].dependency)]
+
 
 @router.get(
     "/{course_id}/announcements",
@@ -32,11 +43,14 @@ def list_course_annoucements(course_id: str, common_paginations: common_page_par
     )
     return response_body
 
+
 @router.post(
     "/{course_id}/announcements",
     status_code=200,
     response_model_exclude_none=True,
 )
-def create_course_annoucement(course_id: str, annoucement_body: PostCourseAnnouncementRequestModel):
+def create_course_annoucement(
+    course_id: str, annoucement_body: PostCourseAnnouncementRequestModel
+):
     created_id = create_course_announcements_response(course_id, annoucement_body)
     return {"announcement_id": created_id}
