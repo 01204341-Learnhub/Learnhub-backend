4e170b3b22d882962af92e3bb1404d5dbd48bf96
sinlnw <sinlnw4567@gmail.com>
2023-09-24 15:12:22 +0700

    add list_class schema

--- learnhub_backend/program/classes/schemas.py
+++ learnhub_backend/program/classes/schemas.py
@@ -1,5 +1,26 @@
 from typing import Optional, Union
 from pydantic import BaseModel, HttpUrl
+from datetime import datetime
 
 class placeholder(BaseModel):
     pass
+
+# CLASSES
+class TeacherModelBody(BaseModel):
+    teacher_id: str
+    teacher_name: str
+class TagModelBody(BaseModel):
+    tag_id: str
+    tag_name: str
+class ListClassesModelBody(BaseModel):
+    class_id: str
+    name: str
+    class_pic: HttpUrl
+    teacher_id: TeacherModelBody
+    status: str
+    tags: list[TagModelBody]
+    registration_ended_date: datetime
+    price: float
+
+class ListClassesResponseModel(BaseModel):
+    classes: list[ListClassesModelBody]
