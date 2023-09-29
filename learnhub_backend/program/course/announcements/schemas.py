3864337b642e375377587688325b687ade9e24bf
sinlnw <sinlnw4567@gmail.com>
2023-09-29 21:24:52 +0700

    get course's announcement.

--- learnhub_backend/program/course/announcements/schemas.py
+++ learnhub_backend/program/course/announcements/schemas.py
@@ -2,24 +2,40 @@ from datetime import datetime
 from typing import Any
 from pydantic import BaseModel, HttpUrl
 
+
 class ListCourseAnnouncementsModelBody(BaseModel):
     announcement_id: str
     last_edit: int
     name: str
 
+
 class ListCourseAnnouncementsResponseModel(BaseModel):
     annoucements: list[ListCourseAnnouncementsModelBody]
+
 
 class AttachmentModelBody(BaseModel):
     attachment_type: str
     src: HttpUrl
 
+
 class PostCourseAnnouncementRequestModel(BaseModel):
     name: str
     text: str
     attachments: list[AttachmentModelBody]
-    #teacher_id: str
+    # teacher_id: str
+
 
 class PostCourseAnnouncementResponseModel(BaseModel):
     announcement_id: str
 
+
+class TeacherModelBody(BaseModel):
+    teacher_id: str
+    teacher_name: str
+class GetCourseAnnouncementResponseModel(BaseModel):
+    announcement_id: str
+    teacher: str # TODO: change to TeacherModelBody
+    name: str
+    last_edit: int
+    text: str
+    attachments: list[AttachmentModelBody]
--- learnhub_backend/program/course/announcements/services.py
+++ learnhub_backend/program/course/announcements/services.py
@@ -1,10 +1,16 @@
 from pydantic import TypeAdapter
-from .database import create_course_announcement, list_course_announcement
+from datetime import datetime
+from .database import (
+    create_course_announcement,
+    list_course_announcement,
+    query_course_announcement,
+)
 from .schemas import (
     ListCourseAnnouncementsModelBody,
     ListCourseAnnouncementsResponseModel,
     PostCourseAnnouncementRequestModel,
     PostCourseAnnouncementResponseModel,
+    GetCourseAnnouncementResponseModel,
 )
 
 
@@ -21,7 +27,22 @@ def list_course_announcements_response(
 
 def create_course_announcements_response(
     course_id: str, request_body: PostCourseAnnouncementRequestModel
-)-> PostCourseAnnouncementResponseModel:
-    created_id = create_course_announcement(course_id=course_id, announcement_body=request_body)
+) -> PostCourseAnnouncementResponseModel:
+    created_id = create_course_announcement(
+        course_id=course_id, announcement_body=request_body
+    )
     response_body = PostCourseAnnouncementResponseModel(announcement_id=created_id)
     return response_body
+
+
+def get_course_announcement_response(course_id: str, announcement_id: str)->GetCourseAnnouncementResponseModel:
+    response_body = query_course_announcement(
+        course_id=course_id, announcement_id=announcement_id
+    )
+    response_body["announcement_id"] = str(response_body["_id"])
+    response_body["teacher"] = str(
+        response_body["teacher_id"]
+    )  # TODO: change to TeacherModelBody
+    response_body["last_edit"] = int(datetime.timestamp(response_body["last_edit"]))
+
+    return GetCourseAnnouncementResponseModel(**response_body)
