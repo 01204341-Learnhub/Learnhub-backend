5252dd40a479ef02d0c1ad2cec271bea1430439e
PAFFx <jj2jom@gmail.com>
2023-09-30 17:40:39 +0700

    finish add teacher

--- learnhub_backend/teacher/database.py
+++ learnhub_backend/teacher/database.py
@@ -1,5 +1,15 @@
-from ..database import db_client
+from learnhub_backend.teacher.schemas import PostTeacherRequestModel
 from bson.objectid import ObjectId
+from bson.errors import InvalidId
+
+from ..database import (
+    db_client,
+)
+from ..dependencies import (
+    teacher_type,
+    Exception,
+)
+
 
 # TEACHERS
 def query_list_teachers(skip: int = 0, limit: int = 100) -> list:
@@ -11,3 +21,38 @@ def query_list_teachers(skip: int = 0, limit: int = 100) -> list:
         teachers.append(teacher)
 
     return teachers
+
+
+def create_teacher(request: PostTeacherRequestModel):
+    try:
+        teacher_body = dict()
+        teacher_body["uid"] = request.uid
+        teacher_body["username"] = request.username
+        teacher_body["email"] = request.email
+        teacher_body["fullname"] = request.fullname
+        teacher_body["profile_pic"] = str(request.profile_pic)
+        teacher_body["config"] = {"theme": "light"}
+        teacher_body["type"] = teacher_type
+        teacher_body["owned_programs"] = []
+
+        # Check duplicate uid
+        uid_filter = {"type": teacher_type, "uid": request.uid}
+        check_uid = db_client.user_coll.find_one(uid_filter)
+        if check_uid != None:
+            error_uid = Exception.unprocessable_content
+            error_uid.__setattr__("detail", "UID duplicate")
+            raise error_uid
+
+        # Check duplicate email
+        email_filter = {"type": teacher_type, "email": request.email}
+        check_email = db_client.user_coll.find_one(email_filter)
+        if check_email != None:
+            error_email = Exception.unprocessable_content
+            error_email.__setattr__("detail", "Email Duplicate")
+            raise error_email
+
+        # Add teacher
+        result = db_client.user_coll.insert_one(teacher_body)
+        return str(result.inserted_id)
+    except InvalidId:
+        raise Exception.bad_request
--- learnhub_backend/teacher/router.py
+++ learnhub_backend/teacher/router.py
@@ -8,11 +8,15 @@ from ..dependencies import (
 
 from .services import (
     list_teachers_response,
+    post_teacher_request,
 )
 
 from .schemas import (
     ListTeachersResponseModel,
+    PostTeacherRequestModel,
+    PostTeacherResponseModel,
 )
+
 router = APIRouter(
     prefix="/users/teachers",
     tags=["teacher"],
@@ -38,4 +42,15 @@ def list_teachers(common_paginations: common_page_params):
         skip=common_paginations["skip"],
         limit=common_paginations["limit"],
     )
+    return response_body
+
+
+@router.post(
+    "/",
+    status_code=200,
+    response_model_exclude_none=True,
+    response_model=PostTeacherResponseModel,
+)
+def post_teacher(request_body: PostTeacherRequestModel):
+    response_body = post_teacher_request(request_body)
     return response_body
