f1212ba28068a6926ad632352e10cc1bf14cff2a
PAFFx <jj2jom@gmail.com>
2023-10-08 16:23:34 +0700

    add post tag

--- learnhub_backend/program/router.py
+++ learnhub_backend/program/router.py
@@ -9,11 +9,14 @@ from ..dependencies import (
 from .schemas import (
     ListProgramsResponseModel,
     ListTagsResponseModel,
+    PostTagRequestModel,
+    PostTagResponseModel,
 )
 
 from .services import (
     list_programs_response,
     list_tags_response,
+    post_tag_request,
 )
 
 
@@ -53,4 +56,15 @@ def list_tags(common_paginations: common_page_params):
     response_body = list_tags_response(
         common_paginations["skip"], common_paginations["limit"]
     )
+    return response_body
+
+
+@router.post(
+    "/tags",
+    status_code=200,
+    response_model=PostTagResponseModel,
+    response_model_exclude_none=True,
+)
+def post_tag(request_body: PostTagRequestModel):
+    response_body = post_tag_request(request_body)
     return response_body
